dc106600b6fb1b72e924c946e7cc42e8ef712548
Xrayya <nur.alkhiyar@gmail.com>
2024-09-19 11:59:20 +0700

    feat(Auth.service): implement tokenization

--- src/services/Auth.service.ts
+++ src/services/Auth.service.ts
@@ -2,33 +2,60 @@ import User from "../entities/User.entity";
 import UserRole from "../enums/UserRole.enum";
 import { AuthenticationException } from "../exceptions/Authentication.exception";
 import UserRepository from "../repositories/User.repository";
+import TokenService from "./Token.service";
 
 class AuthService {
-  constructor(private userRepository: UserRepository) { }
+  constructor(
+    private userRepository: UserRepository,
+    private tokenService: TokenService,
+  ) { }
 
-  async register(name: string, email: string, password: string): Promise<User> {
-    const user = (await this.userRepository.getByEmail(email)) as User;
+  async register(
+    userName: string,
+    userEmail: string,
+    userPassword: string,
+  ): Promise<User> {
+    const user = (await this.userRepository.getByEmail(userEmail, [
+      "role",
+      "password",
+    ])) as Omit<User, "role" | "password">;
 
     if (user) {
       throw new AuthenticationException("User already exists");
     }
 
     return this.userRepository.add({
-      name,
-      email,
-      password,
+      name: userName,
+      email: userEmail,
+      password: userPassword,
       role: UserRole.USER,
     });
   }
 
-  async login(email: string, password: string): Promise<User> {
-    const user = (await this.userRepository.getByEmail(email)) as User;
+  async login(
+    userEmail: string,
+    userPassword: string,
+  ): Promise<{ user: User; token: string }> {
+    const user = (await this.userRepository.getByEmail(userEmail)) as User;
 
-    if (user.password !== password) {
+    if (!user) {
       throw new AuthenticationException("Invalid login credential");
     }
 
-    return user;
+    if (user.password !== userPassword) {
+      throw new AuthenticationException("Invalid login credential");
+    }
+
+    const token = this.tokenService.generateUserToken({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
