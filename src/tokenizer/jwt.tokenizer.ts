f0dd1a6083d67a64dee4a6727e030d08ad7b884b
Xrayya <nur.alkhiyar@gmail.com>
2024-10-31 16:40:44 +0700

    feat(tokenizer): add jwt

---
+++ src/tokenizer/jwt.tokenizer.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import type { ITokenizer } from "./ITokenizer.interface";
+
+class JWTTokenizer implements ITokenizer {
+  encode(data: string | Buffer | object): string {
+    return jwt.sign(data, process.env.JWT_SECRET!);
+  }
+
+  decode(token: string) {
+    return jwt.verify(token, process.env.JWT_SECRET!);
+  }
+}
+
+export default JWTTokenizer;
