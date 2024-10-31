7ef250ddef6ec8ffaf0376035539686a4b52670c
Xrayya <nur.alkhiyar@gmail.com>
2024-10-31 19:26:39 +0700

    docs: leave refactoring comment

--- src/app/routes/auth.ts
+++ src/app/routes/auth.ts
@@ -8,12 +8,16 @@ const authRoute = new Hono()
     const { email, password } = c.req.valid("json");
     const { user, token } = await authService.login(email, password);
 
+    // WARNING: This is a security issue, do not return the password in the response
+    // INFO: One of the possibility is refactor the repository, so the password is not returned
     return c.json({ user, token }, 200);
   })
   .post("/register", ...validateJsonRequest(registerSchema), (c) => {
     const { username, email, password } = c.req.valid("json");
     const user = authService.register(username, email, password);
 
+    // WARNING: This is a security issue, do not return the password in the response
+    // INFO: One of the possibility is refactor the repository, so the password is not returned
     return c.json({ user }, 201);
   });
 
