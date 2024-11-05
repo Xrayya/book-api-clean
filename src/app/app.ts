6e47ab2325e78547a8a0be6d14336944291a73f9
Xrayya <nur.alkhiyar@gmail.com>
2024-11-05 14:28:44 +0700

    feat(app): connect routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,9 @@
 import { Hono } from "hono";
+import { authRoute, booksRoute } from "./routes";
 
 const backendApp = new Hono().basePath("/api");
+
+backendApp.route("/auth", authRoute);
+backendApp.route("/books", booksRoute);
 
 export default backendApp;
