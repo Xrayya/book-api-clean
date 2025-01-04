0b5f8202357b42224efdca3aca5dca0113cd376c
Xrayya <nur.alkhiyar@gmail.com>
2025-01-04 16:17:45 +0700

    feat(IRepository): change parameter type

--- src/domain/interfaces/repositories/IRepository.type.ts
+++ src/domain/interfaces/repositories/IRepository.type.ts
@@ -1,7 +1,7 @@
 import type { IEntity } from "../IEntity.type";
 
 export interface IRepository<T extends IEntity> {
-  get(id: number): Promise<T>;
+  get(id: T["id"]): Promise<T>;
   add(item: Omit<T, "id" | "createdAt" | "updatedAt">): Promise<T>;
   update(id: T["id"], item: Omit<T, "id" | "createdAt" | "updatedAt">): Promise<T>;
   delete(id: T["id"]): Promise<T>;
