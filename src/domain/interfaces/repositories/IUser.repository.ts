1965459b4969ad1adc6d48b7e1e6b14990ac6274
Xrayya <nur.alkhiyar@gmail.com>
2025-01-04 16:28:42 +0700

    refactor(IUserRepository): use overloading

--- src/domain/interfaces/repositories/IUser.repository.ts
+++ src/domain/interfaces/repositories/IUser.repository.ts
@@ -2,7 +2,10 @@ import type User from "@domain/entities/User.entity";
 import type { IRepository } from "./IRepository.type";
 
 export interface IUserRepository extends IRepository<User> {
-  getByEmail(email: User["email"]): Promise<User>;
+  get: {
+    (id: User["id"]): Promise<User>;
+    (email: User["email"]): Promise<User>;
+  }
 
   getPasswordByEmail(email: User["email"]): Promise<User["password"]>;
 }
