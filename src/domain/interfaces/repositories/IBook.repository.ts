ac57c4821b658835e74f46c6a71bdbdcff35c18a
Xrayya <nur.alkhiyar@gmail.com>
2025-01-04 16:22:50 +0700

    feat(IBookRepository): add overriding `get` method

--- src/domain/interfaces/repositories/IBook.repository.ts
+++ src/domain/interfaces/repositories/IBook.repository.ts
@@ -2,15 +2,13 @@ import type Book from "@domain/entities/Book.entity";
 import type { IRepository } from "./IRepository.type";
 
 export interface IBookRepository extends IRepository<Book> {
-  getByTitle(bookTitle: Book["title"]): Promise<Book[]>;
-
-  getFromCategory(category: Book["category"]): Promise<Book[]>;
-
-  getByISBN(ISBN: Book["ISBN"]): Promise<Book>;
-
-  getFromPublisher(publisher: Book["publisher"]): Promise<Book[]>;
-
-  getAvailable(): Promise<Book[]>;
-
-  // getByBookWithAttributeValue(value: string): Promise<Book[]>;
+  get: {
+    (id: Book["id"]): Promise<Book>;
+    (query: {
+      title?: Book["title"];
+      ISBN?: Book["ISBN"];
+      author?: Book["author"];
+      publisher?: Book["publisher"];
+    }): Promise<Book[]>;
+  };
 }
