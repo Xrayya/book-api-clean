85715da0aa9890b567245d8d071f8f7d68c71987
Xrayya <nur.alkhiyar@gmail.com>
2025-01-04 16:41:36 +0700

    feat(IBorrowingRepository): `get` overloading

--- src/domain/interfaces/repositories/IBorrowing.repository.ts
+++ src/domain/interfaces/repositories/IBorrowing.repository.ts
@@ -1,4 +1,12 @@
 import type Borrowing from "@domain/entities/Borrowing.entity";
 import type { IRepository } from "./IRepository.type";
 
-export interface IBorrowingRepository extends IRepository<Borrowing> { }
+export interface IBorrowingRepository extends IRepository<Borrowing> {
+  get: {
+    (id: Borrowing["id"]): Promise<Borrowing>;
+    (
+      userId: Borrowing["user"]["id"],
+      bookId: Borrowing["book"]["id"],
+    ): Promise<Borrowing>;
+  };
+}
