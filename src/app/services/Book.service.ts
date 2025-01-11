a5d81b899c4b1b68025d40088dc831df0fe47edb
Xrayya <nur.alkhiyar@gmail.com>
2025-01-11 18:00:25 +0700

    feat(bookService): add get

--- src/app/services/Book.service.ts
+++ src/app/services/Book.service.ts
@@ -1,5 +1,6 @@
 import type Book from "@domain/entities/Book.entity";
 import type { IBookRepository } from "@domain/interfaces/repositories/IBook.repository";
+import GetBookUseCaseImpl from "@domain/useCases/GetBookImpl.useCase";
 import GetBookListUseCaseImpl from "@domain/useCases/GetBookListImpl.useCase";
 import SearchBooksUseCaseImpl from "@domain/useCases/SearchBooksImpl.useCase";
 
@@ -7,9 +8,11 @@ class BookService {
   constructor(private bookRepository: IBookRepository) {
     this.getBookListUseCase = new GetBookListUseCaseImpl(this.bookRepository);
     this.searchBooksUseCase = new SearchBooksUseCaseImpl(this.bookRepository);
+    this.getBookuseCase = new GetBookUseCaseImpl(this.bookRepository);
   }
 
   private getBookListUseCase: GetBookListUseCaseImpl;
+  private getBookuseCase: GetBookUseCaseImpl;
   private searchBooksUseCase: SearchBooksUseCaseImpl;
 
   async getList(): Promise<Book[]> {
@@ -18,6 +21,10 @@ class BookService {
 
   async search(keyword: string): Promise<Book[]> {
     return this.searchBooksUseCase.execute(keyword);
+  }
+
+  async get(id: Book["id"]): Promise<Book> {
+    return this.getBookuseCase.execute(id);
   }
 }
 
