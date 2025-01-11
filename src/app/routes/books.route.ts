731cbaf9192efe9e07578ac5dffe822e93fb7aea
Xrayya <nur.alkhiyar@gmail.com>
2025-01-11 18:03:12 +0700

    feat(booksRoute): re-implement

--- src/app/routes/books.route.ts
+++ src/app/routes/books.route.ts
@@ -1,47 +1,20 @@
+import { bookService } from "@app/bootstrap";
+import {
+  validateRequest
+} from "@app/middlewares/validation.middleware";
+import { bookDetailsSchema } from "@app/schemas/request/books/bookDetails.schema";
 import { Hono } from "hono";
-import { bookService } from "../../adapter";
-import { validateRequest } from "../middlewares/validation.middleware";
-import { bookDetailsSchema } from "../schemas/request/books/bookDetails.schema";
-import { getBooksSchema } from "../schemas/request/books/books.schema";
 
 export const booksRoute = new Hono()
-  .get("/", ...validateRequest(getBooksSchema), async (c) => {
-    const {
-      search,
-      title,
-      titleStartWith,
-      titleEndWith,
-      ISBN,
-      publisher,
-      category,
-      edition,
-      sortBy,
-      available,
-    } = c.req.valid("query");
+  .basePath("/books")
+  .get("/", async (c) => {
+    const books = await bookService.getList();
 
-    // TODO: Implement the logic to get books by the given parameters using bookService.getBookByParams
-
-    const books = await bookService.getAllBooks(available == "true");
-
-    return c.json(
-      {
-        books,
-      },
-      200,
-    );
+    return c.json({ books }, 200);
   })
-  .get("/:id/details", ...validateRequest(bookDetailsSchema), async (c) => {
-    const { id } = c.req.valid("param");
+  .get("/:id", ...validateRequest(bookDetailsSchema), async (c) => {
+    const id = parseInt(c.req.valid("param").id);
+    const book = await bookService.get(id);
 
-    const { id: bookId, ...bookData } = await bookService.getBookInfoById(
-      parseInt(id),
-    );
-
-    return c.json(
-      {
-        bookId,
-        bookData,
-      },
-      200,
-    );
+    return c.json({ book }, 200);
   });
