20866406fabd928724a2a3508488e7ec6d8e46a7
Xrayya <nur.alkhiyar@gmail.com>
2025-01-21 19:59:35 +0700

    feat(BookEntity): make all properties readonly

--- src/domain/entities/Book.entity.ts
+++ src/domain/entities/Book.entity.ts
@@ -3,17 +3,17 @@ import type { IEntity } from "../interfaces/IEntity.type";
 
 class Book implements IEntity {
   constructor(
-    public id: number,
-    public title: string,
-    public author: string,
-    public ISBN: string,
-    public publisher: string,
-    public publishedYear: number,
-    public category: BookCategory,
-    public edition: number,
-    public available: boolean,
-    public createdAt: Date,
-    public updatedAt: Date,
+    public readonly id: number,
+    public readonly title: string,
+    public readonly author: string,
+    public readonly ISBN: string,
+    public readonly publisher: string,
+    public readonly publishedYear: number,
+    public readonly category: BookCategory,
+    public readonly edition: number,
+    public readonly available: boolean,
+    public readonly createdAt: Date,
+    public readonly updatedAt: Date,
   ) {}
 }
 
