4c150245947073cd5f02336efbea48603afa3c2d
Xrayya <nur.alkhiyar@gmail.com>
2025-01-28 20:36:26 +0700

    test(app): basic test for login

--- src/app/test/auth.test.ts
+++ src/app/test/auth.test.ts
@@ -1,16 +1,17 @@
 import backendApp from "@app/app";
+import JWTTokenizer from "@infrastructure/tokenizer/jwt.tokenizer";
 import { PrismaClient } from "@prisma/client";
 import { describe, expect, test } from "bun:test";
 
-describe("Auth routes", () => {
-  test("should return create user correctly when register with correct credentials", async () => {
-    const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
+describe("Auth routes", () => {
+  test("should return user info correctly when register with correct credentials", async () => {
     try {
       await prisma.user.delete({
         where: { email: "uniquetestemailuser209384091903810923@example.com" },
       });
-    } catch (error) { }
+    } catch (error) {}
 
     const res = await backendApp.request("/api/auth/register", {
       method: "POST",
@@ -26,10 +27,9 @@ describe("Auth routes", () => {
 
     const userCreated = await res.json();
 
-    console.log("Created User", userCreated);
-
     expect(res.status).toBe(201);
     expect(userCreated).toHaveProperty("user");
+    expect(userCreated.user).toBeObject();
     expect(userCreated.user).toHaveProperty("id");
     expect(userCreated.user.id).toBeInteger();
     expect(userCreated.user).toHaveProperty("email");
@@ -48,11 +48,53 @@ describe("Auth routes", () => {
     expect(Date.parse(userCreated.user.createdAt)).not.toBeNaN();
     expect(userCreated.user).toHaveProperty("updatedAt");
     expect(Date.parse(userCreated.user.updatedAt)).not.toBeNaN();
+  });
+
+  test("should return user info and token correctly when login with correct credentials", async () => {
+    const res = await backendApp.request("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "uniquetestemailuser209384091903810923@example.com",
+        password: "Password01",
+      }),
+    });
+
+    const userLoggedIn = await res.json();
+
+    expect(res.status).toBe(200);
+    expect(userLoggedIn).toHaveProperty("user");
+    expect(userLoggedIn.user).toBeObject();
+    expect(userLoggedIn.user).toHaveProperty("id");
+    expect(userLoggedIn.user.id).toBeInteger();
+    expect(userLoggedIn.user).toHaveProperty("email");
+    expect(userLoggedIn.user.email).toBe(
+      "uniquetestemailuser209384091903810923@example.com",
+    );
+    expect(userLoggedIn.user).toHaveProperty("name");
+    expect(userLoggedIn.user.name).toBe("uniquetest");
+    expect(userLoggedIn.user).toHaveProperty("role");
+    expect(userLoggedIn.user.role).toBe("client");
+    expect(userLoggedIn.user).toHaveProperty("isSuspended");
+    expect(userLoggedIn.user.isSuspended).toBe(false);
+    expect(userLoggedIn).toHaveProperty("token");
+    expect(userLoggedIn.token).toBeString();
+
+    const expectedToken = new JWTTokenizer().encode({
+      id: userLoggedIn.user.id,
+      name: userLoggedIn.user.name,
+      email: userLoggedIn.user.email,
+      role: userLoggedIn.user.role,
+    });
+
+    expect(userLoggedIn.token).toBe(expectedToken);
 
     try {
       await prisma.user.delete({
         where: { email: "uniquetestemailuser209384091903810923@example.com" },
       });
-    } catch (error) { }
+    } catch (error) {}
   });
 });
