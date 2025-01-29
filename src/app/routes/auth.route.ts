cda47df603b57a0cdfcaac180e0c068d83c1ece7
Xrayya <nur.alkhiyar@gmail.com>
2025-01-29 16:06:50 +0700

    feat(authRoutes): implement logout

--- src/app/routes/auth.route.ts
+++ src/app/routes/auth.route.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 import { validateJsonRequest } from "../middlewares/validation.middleware";
 import { loginSchema } from "@app/schemas/request/auth/login.schema";
 import { registerSchema } from "@app/schemas/request/auth/register.schema";
+import { verifyAuth } from "@app/middlewares/auth.middleware";
 
 export const authRoute = new Hono()
   .post("/login", ...validateJsonRequest(loginSchema), async (c) => {
@@ -16,4 +17,10 @@ export const authRoute = new Hono()
     const user = await authService.register(username, email, password);
 
     return c.json({ user }, 201);
+  })
+  .get("/logout", verifyAuth, async (c) => {
+    const token = c.req.header().authorization?.split(" ")[1];
+    const isLoggedOut = await authService.logout(token);
+
+    return c.json({ isLoggedOut }, 200);
   });
