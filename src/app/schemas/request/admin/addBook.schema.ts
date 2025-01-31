44381b0b2900b4dc817b501b61aad5e41e5a55f8
Xrayya <nur.alkhiyar@gmail.com>
2025-01-31 17:36:02 +0700

    feat(app.schemas): add add book request schema

---
+++ src/app/schemas/request/admin/addBook.schema.ts
@@ -0,0 +1,27 @@
+import BookCategory from "@domain/enums/BookCategory.enum";
+import { z } from "zod";
+import { BaseRequestSchema } from "../Base";
+
+export const addBookSchema = new BaseRequestSchema({
+  jsonSchema: z.object({
+    title: z.string(),
+    author: z.string(),
+    ISBN: z.string(),
+    publisher: z.string(),
+    publishedYear: z.number().int(),
+    category: z
+      .string()
+      .refine((category) => {
+        const bookCategoryValues = Object.values(BookCategory);
+
+        return bookCategoryValues.includes(category as BookCategory);
+      }, "Invalid category code")
+    ,
+    edition: z.number().int(),
+  }),
+  cookieSchema: z.object({}),
+  formSchema: z.object({}),
+  headerSchema: z.object({}),
+  paramSchema: z.object({}),
+  querySchema: z.object({}),
+})
