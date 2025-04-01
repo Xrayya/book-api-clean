e779d5764e200f35b852f4c44ef94c61ebfe0a1b
Xrayya <nur.alkhiyar@gmail.com>
2025-04-01 08:53:50 +0700

    test(app.borrowing): add test for return book endpoint

--- src/app/test/borrowing.test.ts
+++ src/app/test/borrowing.test.ts
@@ -1,6 +1,7 @@
 import { bookCategoryCodeMapper } from "@/utils";
 import backendApp from "@app/app";
 import { authService } from "@app/bootstrap";
+import type { BorrowedBook } from "@app/services/Borrowing.service";
 import { Book } from "@domain/entities";
 import { BookCategory } from "@domain/enums";
 import { PrismaClient } from "@prisma/client";
@@ -9,6 +10,8 @@ import { afterAll, beforeAll, describe, expect, test } from "bun:test";
 const prisma = new PrismaClient();
 
 let bookData: Book[] = [];
+
+let borrowingData: BorrowedBook[] = [];
 
 const mockBooks: Book[] = [
   new Book(
@@ -221,6 +224,17 @@ describe("Borrowing routes", () => {
         "Password01",
       );
     } catch (error) { }
+
+    try {
+      await prisma.user.create({
+        data: {
+          email: "uniquetestemailadmin1743337516512@example2.com",
+          name: "uniquetestadmin",
+          password: "Password01",
+          role: "ADMIN",
+        },
+      });
+    } catch (error) { }
   });
 
   afterAll(async () => {
@@ -238,11 +252,15 @@ describe("Borrowing routes", () => {
 
     try {
       await prisma.user.delete({
+        where: { email: "uniquetestemailadmin1743337516512@example2.com" },
+      });
+    } catch (error) { }
+
+    try {
+      await prisma.user.delete({
         where: { email: "uniquetestemailuser209384091903810923@example.com" },
       });
-    } catch (error) {
-      console.error(error);
-    }
+    } catch (error) { }
   });
 
   test("should return borrowed books correctly", async () => {
@@ -282,6 +300,8 @@ describe("Borrowing routes", () => {
     });
 
     const resJson = await res.json();
+
+    borrowingData = resJson.borrowedBooks;
 
     expect(res.status).toBe(201);
     expect(resJson).toHaveProperty("borrowedBooks");
@@ -323,5 +343,76 @@ describe("Borrowing routes", () => {
       expect(Date.parse(borrowedBook.borrowDate)).not.toBeNaN();
       expect(borrowedBook).toHaveProperty("returnDate");
     });
+  });
+
+  test("should return returned book info correctly", async () => {
+    const {
+      token,
+      user: { id: userId, name: userName, email: userEmail },
+    } = await authService.login(
+      "uniquetestemailadmin1743337516512@example2.com",
+      "Password01",
+    );
+
+    for (const borrowing of borrowingData) {
+      const res = await backendApp.request("/api/books/borrowing/return", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          bookId: borrowing.bookId,
+          clientId: borrowing.borrower.userId,
+        }),
+      });
+
+      const resJson = await res.json();
+
+      expect(res.status).toBe(200);
+      expect(resJson).toHaveProperty("returnedBook");
+      expect(resJson.returnedBook).toBeObject();
+
+      expect(resJson.returnedBook).toHaveProperty("bookId");
+
+      expect(resJson.returnedBook).toHaveProperty("borrower");
+      expect(resJson.returnedBook).toBeObject();
+
+      expect(resJson.returnedBook.borrower).toHaveProperty("userId");
+      expect(resJson.returnedBook.borrower.userId).toBe(
+        borrowing.borrower.userId,
+      );
+      expect(resJson.returnedBook.borrower).toHaveProperty("userEmail");
+      expect(resJson.returnedBook.borrower.userEmail).toBe(
+        borrowing.borrower.userEmail,
+      );
+      expect(resJson.returnedBook.borrower).toHaveProperty("userName");
+      expect(resJson.returnedBook.borrower.userName).toBe(
+        borrowing.borrower.userName,
+      );
+
+      expect(resJson.returnedBook).toHaveProperty("category");
+      expect(resJson.returnedBook.category).toBe(borrowing.category);
+      expect(resJson.returnedBook).toHaveProperty("title");
+      expect(resJson.returnedBook.title).toBe(borrowing.title);
+      expect(resJson.returnedBook).toHaveProperty("author");
+      expect(resJson.returnedBook.author).toBe(borrowing.author);
+      expect(resJson.returnedBook).toHaveProperty("ISBN");
+      expect(resJson.returnedBook.ISBN).toBe(borrowing.ISBN);
+      expect(resJson.returnedBook).toHaveProperty("publisher");
+      expect(resJson.returnedBook.publisher).toBe(borrowing.publisher);
+      expect(resJson.returnedBook).toHaveProperty("publishedYear");
+      expect(resJson.returnedBook.publishedYear).toBe(borrowing.publishedYear);
+      expect(resJson.returnedBook).toHaveProperty("edition");
+      expect(resJson.returnedBook.edition).toBe(borrowing.edition);
+      expect(resJson.returnedBook).toHaveProperty("createdAt");
+      expect(Date.parse(resJson.returnedBook.createdAt)).not.toBeNaN();
+      expect(resJson.returnedBook).toHaveProperty("updatedAt");
+      expect(Date.parse(resJson.returnedBook.updatedAt)).not.toBeNaN();
+      expect(resJson.returnedBook).toHaveProperty("borrowDate");
+      expect(Date.parse(resJson.returnedBook.borrowDate)).not.toBeNaN();
+      expect(resJson.returnedBook).toHaveProperty("returnDate");
+      expect(Date.parse(resJson.returnedBook.returnDate)).not.toBeNaN();
+    }
   });
 });
